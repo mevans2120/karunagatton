3a727fed32e95467051f0dd786cc5cb80d195677
Michael Evans <evans@beforelabs.com>
2025-04-02 15:59:49 -0700

    Completely rewrite CSS with fallback classes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${ebGaramond.variable} antialiased`}>
-      <body className="min-h-screen bg-background text-gray-800">
+    <html lang="en" className={ebGaramond.variable}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body>
         {children}
       </body>
     </html>
